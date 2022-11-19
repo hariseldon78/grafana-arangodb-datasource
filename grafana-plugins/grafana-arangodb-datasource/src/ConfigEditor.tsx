7d6cd1f5ec5d7a5b73adbc4479f390d4e5279e63
Roberto Previdi <hariseldon78@gmail.com>
2022-11-19 15:00:45 +0100

    started grafana plugin development; start arango with options

--- grafana-plugins/grafana-arangodb-datasource/src/ConfigEditor.tsx
+++ grafana-plugins/grafana-arangodb-datasource/src/ConfigEditor.tsx
@@ -1,83 +1,128 @@
 import React, { ChangeEvent, PureComponent } from 'react';
 import { LegacyForms } from '@grafana/ui';
 import { DataSourcePluginOptionsEditorProps } from '@grafana/data';
-import { MyDataSourceOptions, MySecureJsonData } from './types';
+import { MyDataSourceOptions /* MySecureJsonData */ } from './types';
 
-const { SecretFormField, FormField } = LegacyForms;
+const { /* SecretFormField, */ FormField } = LegacyForms;
 
 interface Props extends DataSourcePluginOptionsEditorProps<MyDataSourceOptions> {}
 
 interface State {}
 
 export class ConfigEditor extends PureComponent<Props, State> {
-  onPathChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const { onOptionsChange, options } = this.props;
-    const jsonData = {
-      ...options.jsonData,
-      path: event.target.value,
-    };
-    onOptionsChange({ ...options, jsonData });
-  };
+    onPropChange =
+        (propName: keyof MyDataSourceOptions) =>
+        (event: ChangeEvent<HTMLInputElement>) => {
+            const { onOptionsChange, options } = this.props;
+            const jsonData: MyDataSourceOptions = {
+                ...options.jsonData,
+            };
+            (jsonData as Record<string, any>)[propName] = event.target.value;
+            onOptionsChange({ ...options, jsonData });
+        };
 
-  // Secure field (only sent to the backend)
-  onAPIKeyChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const { onOptionsChange, options } = this.props;
-    onOptionsChange({
-      ...options,
-      secureJsonData: {
-        apiKey: event.target.value,
-      },
-    });
-  };
+    onDbUrlChange = this.onPropChange('dbUrl');
+    onDbNameChange = this.onPropChange('dbName');
+    onCollectionsRegexChange = this.onPropChange('collectionsRegex');
+    componentDidMount() {
+        this.props.options.jsonData.collectionsRegex = '.*';
+    }
+    /*
+     *     // Secure field (only sent to the backend)
+     *     onAPIKeyChange = (event: ChangeEvent<HTMLInputElement>) => {
+     *         const { onOptionsChange, options } = this.props;
+     *         onOptionsChange({
+     *             ...options,
+     *             secureJsonData: {
+     *                 apiKey: event.target.value,
+     *             },
+     *         });
+     *     };
+     *
+     *     onResetAPIKey = () => {
+     *         const { onOptionsChange, options } = this.props;
+     *         onOptionsChange({
+     *             ...options,
+     *             secureJsonFields: {
+     *                 ...options.secureJsonFields,
+     *                 apiKey: false,
+     *             },
+     *             secureJsonData: {
+     *                 ...options.secureJsonData,
+     *                 apiKey: '',
+     *             },
+     *         });
+     *     };
+     *  */
 
-  onResetAPIKey = () => {
-    const { onOptionsChange, options } = this.props;
-    onOptionsChange({
-      ...options,
-      secureJsonFields: {
-        ...options.secureJsonFields,
-        apiKey: false,
-      },
-      secureJsonData: {
-        ...options.secureJsonData,
-        apiKey: '',
-      },
-    });
-  };
+    render() {
+        const { options } = this.props;
+        const { jsonData /* secureJsonFields */ } = options;
+        /* const secureJsonData = (options.secureJsonData || {}) as MySecureJsonData; */
 
-  render() {
-    const { options } = this.props;
-    const { jsonData, secureJsonFields } = options;
-    const secureJsonData = (options.secureJsonData || {}) as MySecureJsonData;
+        return (
+            <div className="gf-form-group">
+                <div className="gf-form">
+                    <p>No authentication support for now</p>
+                </div>
+                <div className="gf-form">
+                    <FormField
+                        label="Db url"
+                        labelWidth={6}
+                        inputWidth={20}
+                        onChange={this.onDbUrlChange}
+                        value={jsonData.dbUrl || ''}
+                        placeholder="http://arango:8529"
+                    />
+                </div>
+                <div className="gf-form">
+                    <FormField
+                        label="Db name"
+                        labelWidth={6}
+                        inputWidth={20}
+                        onChange={this.onDbNameChange}
+                        value={jsonData.dbName || ''}
+                        placeholder="test_database"
+                    />
+                </div>
+                <div className="gf-form">
+                    <FormField
+                        label="collectionsRegex"
+                        labelWidth={6}
+                        inputWidth={20}
+                        onChange={this.onDbUrlChange}
+                        value={jsonData.collectionsRegex || '.*'}
+                        placeholder=".*"
+                    />
+                </div>
+                {/* <div className="gf-form">
+                    <FormField
+                    label="Path"
+                    labelWidth={6}
+                    inputWidth={20}
+                    onChange={this.onPathChange}
+                    value={jsonData.path || ''}
+                    placeholder="json field returned to frontend"
+                    />
+                    </div>
 
-    return (
-      <div className="gf-form-group">
-        <div className="gf-form">
-          <FormField
-            label="Path"
-            labelWidth={6}
-            inputWidth={20}
-            onChange={this.onPathChange}
-            value={jsonData.path || ''}
-            placeholder="json field returned to frontend"
-          />
-        </div>
-
-        <div className="gf-form-inline">
-          <div className="gf-form">
-            <SecretFormField
-              isConfigured={(secureJsonFields && secureJsonFields.apiKey) as boolean}
-              value={secureJsonData.apiKey || ''}
-              label="API Key"
-              placeholder="secure json field (backend only)"
-              labelWidth={6}
-              inputWidth={20}
-              onReset={this.onResetAPIKey}
-              onChange={this.onAPIKeyChange}
-            />
-          </div>
-        </div>
-      </div>
-    );
-  }
+                    <div className="gf-form-inline">
+                    <div className="gf-form">
+                    <SecretFormField
+                    isConfigured={
+                    (secureJsonFields && secureJsonFields.apiKey) as boolean
+                    }
+                    value={secureJsonData.apiKey || ''}
+                    label="API Key"
+                    placeholder="secure json field (backend only)"
+                    labelWidth={6}
+                    inputWidth={20}
+                    onReset={this.onResetAPIKey}
+                    onChange={this.onAPIKeyChange}
+                    />
+                    </div>
+                    </div> */}
+            </div>
+        );
+    }
 }
--- grafana-plugins/grafana-arangodb-datasource/src/datasource.ts
+++ grafana-plugins/grafana-arangodb-datasource/src/datasource.ts
@@ -1,4 +1,7 @@
 import defaults from 'lodash/defaults';
+import { Database } from 'arangojs';
+import { CollectionMetadata } from 'arangojs/collection';
+import _ from 'lodash';
 
 import {
     DataQueryRequest,
@@ -12,11 +15,27 @@ import {
 import { MyQuery, MyDataSourceOptions, defaultQuery } from './types';
 
 export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
-    constructor(instanceSettings: DataSourceInstanceSettings<MyDataSourceOptions>) {
+    arango: Database;
+    collections: CollectionMetadata[] = [];
+    constructor(
+        public instanceSettings: DataSourceInstanceSettings<MyDataSourceOptions>
+    ) {
         super(instanceSettings);
+        const { dbUrl: url, dbName: databaseName } = this.instanceSettings.jsonData;
+        console.log({ url, databaseName });
+        this.arango = new Database({ url, databaseName });
+        void this.setup();
+    }
+    async setup() {
+        const { collectionsRegex } = this.instanceSettings.jsonData;
+        this.collections = (await this.arango.listCollections()).filter((c) =>
+            c.name.match(collectionsRegex)
+        );
+        console.log(this.collections);
     }
 
     async query(options: DataQueryRequest<MyQuery>): Promise<DataQueryResponse> {
+        console.log('ArangoDataSource query called:', { options });
         const { range } = options;
         const from_ = range!.from.valueOf();
         const to = range!.to.valueOf();
@@ -53,10 +72,48 @@ export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
     }
 
     async testDatasource() {
+        const {
+            collectionsRegex,
+            dbUrl: url,
+            dbName: databaseName,
+        } = this.instanceSettings.jsonData;
         // Implement a health check for your data source.
+
+        // try {
+        const arango = new Database({ url, databaseName });
+        // const collections = (await arango.listCollections()).filter((c) =>
+        //     c.name.match(collectionsRegex)
+        // );
+        let info = 'error';
+        try {
+            console.log(arango.get());
+            info = (await arango.get()).name;
+        } catch (e: any) {
+            info = e.message;
+        }
         return {
             status: 'success',
-            message: 'Success',
+            message:
+                'Success ' +
+                JSON.stringify({
+                    url,
+                    databaseName,
+                    collectionsRegex,
+                    // collections: collections.map((c) => c.name),o
+                    info: info,
+                }),
         };
+        // } catch (err) {
+        //     let message = 'Cannot connect';
+        //     if (_.isString(err)) {
+        //         message = err;
+        //     } else if ((err as Record<string, any>)['message']) {
+        //         message = (err as Record<string, any>)['message'];
+        //     }
+        //     return {
+        //         status: 'error',
+        //         message,
+        //     };
+        // }
     }
 }
